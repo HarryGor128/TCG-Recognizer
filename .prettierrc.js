58f2113034f81df4a2cb47a27042d4258c399b98
HarryGor128 <harry99128@gmail.com>
2023-09-13 01:07:01 +0800

    prettierrc fix

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,9 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: 'auto',
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': 0
+  },
 };
---
+++ scr/AppContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const AppContainer = () => {
+  return <View></View>;
+};
+
+export default AppContainer;
