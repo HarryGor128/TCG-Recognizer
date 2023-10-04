cf2a7c15f5461a1256b14e0007bf46e8346b46b6
HarryGor128 <harry99128@gmail.com>
2023-10-04 10:38:48 +0800

    Fix app crash

--- scr/Page/GoogleResult/GoogleResultScreen.tsx
+++ scr/Page/GoogleResult/GoogleResultScreen.tsx
@@ -2,22 +2,23 @@ import { Text, View } from 'react-native';
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
+import useAndroidBackButton from '../../Hook/Common/useAndroidBackButton';
 import ScreenParamList from '../../Type/Navigation/ScreenParamList';
 
 type NavigationProps = NativeStackScreenProps<ScreenParamList, 'GoogleResult'>;
 
-const GoogleResultScreen = ({ route }: NavigationProps) => {
+const GoogleResultScreen = ({ route, navigation }: NavigationProps) => {
     const { ScanningResult } = route.params;
     console.log(
         '🚀 ~ file: GoogleResultScreen.tsx:11 ~ GoogleResultScreen ~ ScanningResult:',
         ScanningResult,
     );
 
-    return (
-        <View style={{ flex: 1, backgroundColor: 'white' }}>
-            <Text>{ScanningResult}</Text>
-        </View>
-    );
+    useAndroidBackButton(() => {
+        navigation.goBack();
+    });
+
+    return <></>;
 };
 
 export default GoogleResultScreen;
